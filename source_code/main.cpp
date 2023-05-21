ab21b38c9b6a2218354dcfb31593d96931f16bb1
ZiongaB <91677985+ZiongaB@users.noreply.github.com>
2023-05-20 21:13:13 -0400

    -Working on fixing printing
    Just saving this for later not finished with making printing to file work properly

--- source_code/main.cpp
+++ source_code/main.cpp
@@ -5,31 +5,71 @@
 #include <fstream>
 #include <math.h>
 #include <vector>
+#include <sstream>
 #include "flow_network_generator.h"
 using namespace std;
 
 
 int main () {
 
-  Graph a = GenerateNetwork("Test Graph");
+  for(int i = 0; i < 10; i++)
+  {
+    cout << "-------------------------------------------------"<< endl;
+    Graph a = GenerateNetwork("Graph " + to_string(i+1));
+    cout << "-------------------------------------------------"<< endl;
+    cout<< "Starting print to file"<< endl;
+    int vertex = a.V.size();
+    int edge = a.E.size();
+    cout<< "-------------------------------------------------"<< endl;
 
-  int vertex = a.V.size();
-  int edge = a.E.size();
-  cout<< "-------------------------------------------------"<< endl;
-  // run for loop from 0 to vecSize
-  for(int i = 0; i < vertex; i++)
-  {
-    cout << "Graph vertex: " << a.V[i].name << endl;
-  }
-  for (int l = 0; l< edge; l++)
-  {
-    cout<< "Edge from " << a.E[l].begin << " to " << a.E[l].end<< " and has a flow of " << a.E[l].flow << endl;
-  }
+    
+    /*for(int i = 0; i < vertex; i++)
+    {
+      cout << "Graph vertex: " << a.V[i].name << endl;
+    }
+    for (int l = 0; l< edge; l++)
+    {
+      cout<< "Edge from " << a.E[l].begin << " to " << a.E[l].end<< " and has a flow of " << a.E[l].flow << endl;
+    }*/
+
+    string graphFile = "input_graphs/graph";
+    graphFile = graphFile+to_string(i+1);
+    graphFile = graphFile+".dot";
+
+    cout<< graphFile<< endl;
+
+    string fileText ="diagraph g{\n\nrankdir=LR\n\n";
+    string number =to_string(i+1);
+    cout<< fileText<< endl;
+ 
+    for (int l = 0; l< edge; l++)
+    {
+      cout << a.E[l].begin<< endl;
+      string begin(1, a.E[l].begin);
+      string end(1, a.E[l].end);
+      string flow = to_string(a.E[l].flow);
+
+      //string begin = a.E[l].begin;
+      //string end = a.E[l].end;
+      //string addLine = a.E[l].begin+" -> "+a.E[l].end+' [label = " '+a.E[l].flow+' "];\n';
+      //cout<< addLine<< endl;
+      //fileText =fileText.append(addLine);
+    }
+
+    string endFile ="\nlabel = \"graph\""+number+"\"];\n}";
+    cout<< endFile<< endl;
+
+    //fileText=fileText.append(endFile);
+    //cout<< fileText<< endl;
+    
+
+
+    ofstream myfile;  
+    myfile.open (graphFile);
+    myfile << "Test message"; 
+    myfile.close();
+    
   
-  
-  ofstream myfile;  
-  myfile.open ("example.dot");
-  myfile << "Writing this to a file.\n"; 
-  myfile.close(); 
+  }
   return 0;
 }
